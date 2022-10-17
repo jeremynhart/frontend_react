fb14a4764efe00a274c5f1f555f98f4c146f2e79
Jeremy Hart <jeremynhart@gmail.com>
2022-10-17 11:58:05 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jeremy is a Badass!!!
         </a>
       </header>
     </div>
